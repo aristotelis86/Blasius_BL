7853979dfe4db4a1ac40e3756def6c19c288512b
Aristotelis Liakatas <agliak@gmail.com>
2017-02-21 14:21:05 +0000

    Adding Pohlhausen profile.

--- Blasius.py
+++ Blasius.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 def Blasius(f,t):
     return [f[1], f[2], -f[0]*f[2]/2]
 
-def derivative(f, x, h):
-    deriv = (1.0/(2*h))*(f(x+h)-f(x-h))
-    return deriv
+def Pohl(x):
+    return (2/5.8356)*x-(2/5.8356**3)*x**3+(1/5.8356**4)*x**4
 
 AA = 0.;
 BB = 1.;
-tol = 0.00001
+tol = 1e-8
 maxiter = 100
-z = numpy.linspace(0,7,200)
+#z = numpy.linspace(0,5.8356,100)
+z = numpy.linspace(0,10,1000)
 
 CC = (AA+BB)/2.0
 iter = 0
@@ -42,8 +42,18 @@ while (BB-AA)/2.0>tol and iter<maxiter:
 
 print("iterations needed=",iter)
 print("best initial value=",CC)	 
-         
-plt.plot(Aprime[:,1],z)
+
+Ainit=[0., 0., CC]
+Aprime = integrate.odeint(Blasius,Ainit,z)
+
+
+plt.figure(figsize=(5,5)) 
+plt.plot(Aprime[:,1],z,label='Blasius')
+plt.plot(Pohl(z),z,label='Pohlhausen')
+plt.xlim([0., 1.])
+plt.xlabel("u/U")
+plt.ylabel("z")
+plt.legend()
 plt.savefig('Blasius_profile.png')
 
 
